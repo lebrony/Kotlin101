9ba19b85dc9f918d76b9e8226cebad651478e272
lebrony <asandler778@gmail.com>
2022-02-04 19:41:07 +0300

    convert data types

---
+++ src/ConvertDataType.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>){
+    var num1 = 10
+    var num2:Int?
+    var num2Str:String = "200"
+    num2 = num2Str.toInt()
+    var num3:Float?
+    num3 = num2Str.toFloat()
+
+    println("number 1 :"+ num1)
+    println("number 2 :"+num2)
+    println("number 3 :"+num3)
+}
