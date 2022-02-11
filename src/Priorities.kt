b01b06e7c0d30bfcb793b5a8c18ad9347f14ed41
lebrony <asandler778@gmail.com>
2022-02-11 20:16:24 +0300

    convert data types

---
+++ src/Priorities.kt
@@ -0,0 +1,15 @@
+//Operation rules or Priorities
+//( )
+//^
+//* , /
+//+ , -
+//=
+
+fun main(args:Array<String>){
+    var num1:Int = 20
+    var num2:Int = 10
+    var num3:Int = 4
+    var sum:Int?
+        sum=num1+num2*num3-num2
+    print("Summation:"+sum)
+}
